5f73cc68c29604fdebeffc31159c687a40803568
loartan <loartgames@gmail.com>
2025-10-08 20:08:46 +0300

    Fix: сумму денег записываю не в переменную, а в список

--- banda.py
+++ banda.py
@@ -1,15 +1,15 @@
 count_members = int(input("Сколько в банде члэнов? "))
 band = []
-total_money = 0
+money_band = []
 
 for i in range(1, count_members + 1):
     name = input(f"Введи имя члэна {i}: ")
     band.append(name)
     money = int(input(f"Сколько занес {name}? "))
-    total_money += money
+    money_band.append(money)
     
 print("--- ИТОГИ СХОДКИ ---")
 print(f"Всего бойцов: {len(band)}")
 print(f"Наша братва: {band}")
-print("Общак:", total_money)
-print(f"В среднем каждый скинулся по {total_money/len(band)}")
+print("Общак:", sum(money_band))
+print(f"В среднем каждый скинулся по {sum(money_band)/len(band)}")
