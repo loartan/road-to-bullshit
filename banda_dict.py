b2296e8487a28dfc31d862c0f12b2c691c77acca
loartan <loartgames@gmail.com>
2025-10-08 20:39:34 +0300

    Урок 4. Немного растерялся, как вывести самого богатого члена, но нашел решение

---
+++ banda_dict.py
@@ -0,0 +1,19 @@
+my_dict = {}
+count_members = int(input("Сколько членов в нашей банде? "))
+
+for i in range(count_members):
+    name = input(f"Введите имя члэна {i+1}: ")
+    money = int(input(f"Сколько внес {name}? "))
+    my_dict[name] = money
+    
+most_money = max(my_dict.values())
+rich_member = ""
+for member in my_dict.keys():
+    if my_dict[member] == most_money:
+        rich_member = member
+print("--- ИТОГИ СХОДКИ ---")
+print(f"Количество члэнов в банде: {len(my_dict)}")
+print(f"Список члэнов: {my_dict.keys()}")
+print(f"Общая сумма в сейфе: {sum(my_dict.values())}")
+print(f"Больше всех занес: {rich_member}")
+print(f"Этот член внес: {most_money}")
