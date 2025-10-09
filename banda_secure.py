eee9eafcff90fa5832d285f78909905011a66acf
loartan <loartgames@gmail.com>
2025-10-09 20:15:21 +0300

    Урок 6: Отработали ошибки

---
+++ banda_secure.py
@@ -0,0 +1,31 @@
+def collecting_data():
+    print("--- НАЧИНАЕМ СБОР ДАНИ ---")
+    band_data = {}
+    count = int(input("Сколько членов в банде? "))
+    for i in range(count):
+        name = input(f"Как зовут члена {i+1}? ")
+        try:
+            money = int(input(f"Сколько внес {name}? "))
+        except ValueError:
+            print(f"{name} - это не цифры, еблан! Раздевайся твою мать, будем тебя ебать. Записываем 0!") 
+            money = 0
+        band_data[name] = money
+            
+    return band_data
+
+def analizing_and_report(data_dict: dict):
+    print("\n--- АНАЛИТИЧЕСКИЙ ОТДЕЛ ДОКЛАДЫВАЕТ ---")
+    count = len(data_dict)
+    total = sum(data_dict.values())
+    rich = max(data_dict, key=data_dict.get)
+    max_amount = data_dict[rich]
+
+    print(f"Количество членов: {count}")
+    print(f"Общая сумма: {total}")
+    print(f"Член, который внес больше всех: {rich}")
+    print(f"Он внес: {max_amount}")
+
+our_band = collecting_data()
+print(f"DEBUG: Собрано: {our_band}")
+analizing_and_report(our_band)
+print("\n--- Сходка окончена, расходимся ---")
